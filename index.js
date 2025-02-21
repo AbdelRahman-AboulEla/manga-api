8af9da43524460e31101f3549b82d4ec5177cec1
AbdelRahmanAboulEla <126163624+AbdelRahmanAboulEla@users.noreply.github.com>
2025-02-22 01:08:47 +0200

    ..

--- index.js
+++ index.js
@@ -12,18 +12,12 @@ const port = 3000;
 async function getChapterImages(mangaSlug, chapterNumber) {
   const url = `https://lekmanga.net/manga/${mangaSlug}/${chapterNumber}`;
   try {
-    const response = await axios.get(url, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-      },
-    });
-    const $ = load(response.data); // Use load instead of cheerio.load
+    const response = await axios.get(url);
+    const $ = load(response.data); 
 
-    // Extract image URLs (adjust selector based on website structure)
     const imgTags = $(
       ".reading-content .page-break.no-gaps img.wp-manga-chapter-img"
-    ); // Example selector
+    ); 
     const imageUrls = [];
     imgTags.each((index, element) => {
       imageUrls.push($(element).attr("src"));
